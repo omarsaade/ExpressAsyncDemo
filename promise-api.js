bd19f70bbaf172552ad54060e6770d23e730ccb6
omar saadeh <omarsaadeh11@gmail.com>
2022-12-05 12:49:28 -0800

    Creating Settled Promises

--- promise-api.js
+++ promise-api.js
@@ -1,8 +1,17 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+  }, 2000);
+});
 
-const po = Promise.reject(new Error("reason for rejection..."));
-po.then(null, (result) => console.log(result));
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
 
-const pos = Promise.reject(new Error("reason for rejection..."));
-pos.catch((error) => console.log(error));
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err.message));
--- promise.js
+++ promise.js
@@ -1,39 +1,30 @@
-/*
 console.log("Before");
-//asynchronous code that uses that callback approach
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    });
-  });
-});
-console.log("After");
-*/
 
-//write this using promises
-console.log("Before");
-//asynchronous code that uses that callback approach
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
 
-// const p = getUser(1);
-// p.then((user) => console.log(user));
+//Async and Await approach
 
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
-
-console.log("After");
-
-/*
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({ id: id, gitHubUsername: "mosh" });
-  }, 2000);
+//thats an asychronous code that looks that synchronous.
+//the best
+async function displayCommits() {
+  try {
+    //                 getUser(1) return a promise , we can await the result
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (error) {
+    console.log("Error", err.message);
+  }
 }
-*/
+
+displayCommits();
+
+console.log("After");
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
@@ -44,15 +35,6 @@ function getUser(id) {
   });
 }
 
-/*
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("Calling GitHub API...");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
-}
-*/
-
 function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -61,15 +43,6 @@ function getRepositories(username) {
     }, 2000);
   });
 }
-
-/*
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log("Calling GitHub API...");
-    callback(["commit"]);
-  }, 2000);
-}
-*/
 
 function getCommits(repo) {
   return new Promise((resolve, reject) => {
